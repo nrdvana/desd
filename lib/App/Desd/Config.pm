24e9454823aaa1864750cc18261d5e56d5485ffc
Michael Conrad <mike@nrdvana.net>
2015-01-25 19:04:00 -0500

    Reworked the structure of the config file
    Service start action no longer becomes the service, instead there
    is a 'run' attribute for the service itself, and the start action
    calls to an internal method to do the exec, and then returns a
    code indicating success or failure.
    
    Changed 'set-mode' to 'goal', and now actions only apply a goal
    if they are called directly by the user.
    
    Run values can now be maps and specify internal calls.
    
    Killscript can now happen without forking at all, which will be
    much more efficient.
    
    Control handles are no longer given to actions by default.
    
    Reserved some attributes for the yet-to-be-defined permission
    system.
    
    Specified 'check' as a standard action, which defaults to a check
    if the service has been running for 2 seconds.

--- lib/App/Desd/Config.pm
+++ lib/App/Desd/Config.pm
@@ -3,28 +3,23 @@ use Moo;
 
 =head1 SYNOPSIS
 
-  $config= App::Desd::Config->deserialize(file => $path);
+  $config= App::Desd::Config->new_from_file(file => $path);
   $config= App::Desd::Config->new(\%data);
   
   print $config->service('foo')->args;
   
-  $config->serialize(file => $path);
-  $config->serialize(data => \my $data);
-  $yaml= $config->serialize(yaml => 1);
+  $yaml= $config->serialize();
+  $config->serialize_to_file($path_or_handle);
 
 =head1 CONFIGURATION STRUCTURE
 
 The config file for Desd is YAML-encoded structured data:
 
-=over
-
-=item service NAME:
+=head2 service NAME
 
 Defines a service named NAME.
 
-=over
-
-=item env:
+=head3 env
 
 A key/value map of environment variables that will be applied for any program
 or script that is executed in relation to this service.
@@ -54,82 +49,144 @@ The path to the base directory of this Desd instance.
 
 =back
 
-=item action NAME:
+=head3 goal
 
-Actions describe a thing that you want to do to a service.  Think of them like
-methods.  Typical actions are "start", "stop", "restart".  Other common actions
-are "reload" or "check".  These are the names you use from desctl to manipulate
-the service from the commandline.
-
-An action can be a single string (described below) or a key/value set of the
-following:
+Specifies the initial goal for the service.  Each service has a goal which is one of:
 
 =over
 
-=item run:
+=item up
+
+If the service is not running, keep executing the start action until it is.
+
+=item down
+
+If the service is running, keep executing the stop action until it exits.
+
+=item once
+
+If the service is not running, start it.  When it stops for any reason, change the
+goal to 'down'.
+
+=item cycle
+
+If the service is running, run the stop action until it exits.  Once it stops
+(or if it was initially down) change the goal to 'up'.
+
+=back
+
+=head3 run
 
 The program to execute.  If you specify a string and it contains shell meta
 characters it will be passed off to the shell.  If it contains whitspace it
 will be split directly and passed to exec(), with automatic argv0.  If you
 specify an array it will be pased to exec() with automatic argv0.
 
-=item argv0:
+=head3 argv0
 
-If you need to override argv[0] in the call to run, specify this parameter.
+If you need to override argv[0] in the call to exec(), specify this parameter.
 
-=item io:
+=head3 io
 
 The array of file descriptors to pass to the program.  These are named handles,
 as defined in the rest of your desd configuration.  The default is to use the
-handles defined for the service, which default to [ null, log, log, desd_comm ],
-where log is the logger for this service and desd_comm is the control socket
-for this service to make API requests to desd.
+handles defined for the service, which default to [ null, log, log ],
+where log is the logger for this service.  (If the service does not define a logger
+then it is the desd logger, and if desd doesn't define a logger it is desd's STDERR.)
 
-=item env:
+Another common handle alias is 'desd_comm' which gives the script a socket to
+communicate with desd to make API calls.
+
+=head3 action NAME
+
+Actions describe a thing that you want to do to a service.  Think of them like
+methods called on a service.  Some actions are built-in, but you can define any
+verb that makes sense for your service.
+
+Each action can have the following attributes:
+
+=over
+
+=item run
+
+Same as for a service.  In addition, you can call desd's internal functionality
+with the notation
+
+  { internal: ['method','arg1','arg2',...] }
+
+=item env
 
 Like the service's env, this alters the environment of any script executed
-by this action.
+by this action.  Defaults to the same env as the service itself.
 
-=item set-mode:
+=item goal
 
-Change the mode of the service at the start of this action.  Modes are
-'up', 'down', 'once', or "cycle".  (more might be added later)
+When this action is invoked directly by a user, the goal of the service will be
+changed to this value.
+
+=item parallel
+
+By default, only one action may run on a service at a time.  If you want an action
+to be able to run in parallel with another action, list those actions in this
+attribute.
+
+=item access
+
+Specify an array of access tokens which this action is given permission to use.
+The permission system has not been fully specified yet, so this is a place holder
+for now.
 
 =back
 
-Some actions have special defaults:
+The built-in actions are:
 
 =over
 
 =item start
 
-This action has a default 'io' of [null, log, log].
-(the "desd_comm" handle on FD 3 is not present.)  This is because most start
-scripts probably don't need it, and it would be a waste to leave that socket
-open for the entire duration of the service.
+The default "goal" is "up", so that the service gets restarted if it dies.
 
-It also has a default "set-mode" of "up", so that the service gets restarted
-if it dies.
+The default "run" is the internal method "exec_unless_running", which execs the
+service unless it is running.
 
 =item stop
 
-This action has a default "set-mode" of "down", so that the service no longer
-gets restarted.
+The default "goal" is "down", so that the service no longer gets restarted.
 
-It has a default "killscript" of "SIGTERM SIGCONT 30 SIGTERM 20 SIGQUIT 5 SIGKILL".
-Note that if you don't want the killscript, you need to set it to null in
-addition to adding your "run" parameters.
+The default "run" is the internal method "killscript" with arguments
+
+  SIGTERM SIGCONT 30 SIGTERM 20 SIGQUIT 5 SIGKILL 20
 
 =item restart
 
-This action has "set-mode" of "cycle".  This has the effect of running the stop
-action if it was running, and then running the start action.
+The default "goal" is "cycle".  This has the effect of running the stop
+action if the service was running, and then running the start action.
+
+The default "run" is the internal method "stop_start", which directly calls stop
+and then calls "start", returning true if both those direct commands succeed.
+(if they fail, the restart fails, but desd will still try to restart the service
+due to the goal of "cycle").
+
+=item check
+
+This should be overridden with some test script that verifies the service is
+performing the duties it is supposed to.
+
+The default "parallel" is "*" meaning this action may run at any time.
+
+The default "run" is the internal method "wait_for_uptime 3".
 
 =back
 
-=back
+=head2 signal NAME(s)
 
-=back
+(not yet specified)
+
+=head2 auth TOKEN
+
+Specify a set of permissions that are awarded to any client/service with TOKEN.
+
+(not yet specified)
 
 =head1 METHODS
 
