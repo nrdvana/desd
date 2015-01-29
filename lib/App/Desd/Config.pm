71312034289e72652af83bf388bbd8bb81ead90e
Michael Conrad <mike@nrdvana.net>
2015-01-29 15:56:00 +0500

    Reduced the scope of the config object
    I will take care of serialization from the main program

--- lib/App/Desd/Config.pm
+++ lib/App/Desd/Config.pm
@@ -8,9 +8,6 @@ use Moo;
   
   print $config->service('foo')->args;
   
-  $yaml= $config->serialize();
-  $config->serialize_to_file($path_or_handle);
-
 =head1 DESCRIPTION
 
 App::Desd::Config is a class that handles the details of reading the config
@@ -28,52 +25,6 @@ L<App::Desd::Manual::ConfigFile>.
 =head2 new
 
 Standard Moo constructor.  Warns about unknown arguments.
-
-=head2 get_ctor_args
-
-Return perl data which could be used to reconstruct a config object.
-
-=head2 deserialize
-
-  $config= App::Desd::Config->deserialize(file => $path);
-  $config= App::Desd::Config->deserialize(yaml => $yaml);
-
-Load a config from some serialized form.  If loading from file,
-the format will be auto-detected.  The only format right now
-is YAML.
-
-Calls L<new> and returns an object.
-
-=head2 serialize
-
-  my $result= $config->serialize( %options )
-
-  my $yaml= $config->serialize(yaml => 1);
-  my $data= $config->serialize(new_args => 1);
-  $config->serialize(file => $path);
-
-Serialize the configuration, either to a yaml string or to a file
-(in yaml format).
-
-=over
-
-=item new_args
-
-Return un-blessed perl data, which could be passed to 'new' to
-clone this config.
-
-=item yaml
-
-The value of this key is either 1 (meaning return the yaml) or a scalar ref
-which receives the yaml.  The yaml text can be passed to L<deserialize> to
-clone this config.
-
-=item file
-
-The value of this key is a file name, which will be overwritten with the YAML
-serialization of the config, or a file handle which will be written directly.
-
-=back
 
 =head2 service
 
