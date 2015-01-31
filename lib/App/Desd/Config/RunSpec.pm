0185824a9aa14963cdd8091509fba38c6887fdf4
Michael Conrad <mike@nrdvana.net>
2015-01-31 21:18:00 +0500

    Improved RunSpec specification and coercion

--- lib/App/Desd/Config/RunSpec.pm
+++ lib/App/Desd/Config/RunSpec.pm
@@ -12,55 +12,56 @@ a service.
 =cut
 
 has 'internal', is => 'ro';
-has 'exec',     is => 'ro';
+has 'exec',     is => 'ro', isa => NonemptyArrayOfLazyScalar, is => 'ro';
+
+sub BUILD {
+	my $self= shift;
+	defined($self->internal) + defined($self->exec) == 1
+		or die "Run must specify 'internal:' xor 'exec:'\n";
+}
 
 sub _coerce_run {
 	my $thing= shift;
 	defined $thing or die "Run specification cannot be empty\n";
 	if (!ref $thing) {
-		# If run is a string, and starts with SIG\w+, it's a killscript
-		if ($thing =~ /^SIG[A-Z0-9]+/) {
-			return { internal => [ 'killscript', split(/\s+/, $thing) ] };
-		}
-		# If run is a string, and starts with *WORD,
-		#  split on whitespace and make it an internal call
-		if ($thing =~ /^\*(\w+.*)/) {
-			return { internal => [ split(/\s+/, $1) ] };
-		}
-		# If run is a string, and contains shell characters,
-		#   convert it to call to $SHELL
-		if ($thing =~ /[^\w\s_=\/.:,-]/) {
-			return { exec => [ \'SHELL', \'SHELL', '-c', $thing ] };
-		}
-		# Else split on whitespace and make it the args to exec()
-		my @argv= split /\s+/, $thing;
-		@argv > 0 or die "Must have at least one argument\n";
-		return { exec => [ $argv[0], @argv ] };
+		# Process any special interpretation for the string
+		$thing= _interpret_run_string($thing);
 	}
-	# If run is an array, then treat it as argv
-	if (ref $thing eq 'ARRAY') {
-		NonemptyArrayOfScalar->check($thing)
-			or die "run must be non-empty array of argument strings\n";
-		return { exec => [ $thing->[0], @$thing ] };
+	elsif (ref $thing eq 'ARRAY') {
+		# If run is an array, then treat it as argv
+		$thing= { args => $thing };
 	}
-	if (ref $thing eq 'HASH') {
-		if (defined $thing->{args}) {
-			NonemptyArrayOfScalar->check($thing->{args})
-				or die "args must be non-empty array of argument strings\n";
-			$thing->{exec}= [ $thing->{args}[0], @{$thing->{args}} ];
-		}
-		defined($thing->{internal}) + defined($thing->{exec}) == 1
-			or die "Run must specify 'internal:' xor 'exec:'\n";
-		if (defined $thing->{exec}) {
-			NonemptyArrayOfScalar->check($thing->{exec})
-				or die "exec must be non-empty array of argument strings\n";
-			return { exec => $thing->{exec} };
-		}
-		else {
-			return { internal => $thing->{internal} };
-		}
+	elsif (ref $thing ne 'HASH') {
+		# ensure hashref
+		die "Don't know how to convert ".ref($thing)." to run specification\n";
 	}
-	die "Don't know how to convert ".ref($thing)." to run specification\n";
+	# convert 'args' to 'exec'
+	if (defined my $args= delete $thing->{args}) {
+		$thing->{exec}= [ $args->[0], @$args ];
+	}
+	return __PACKAGE__->new($thing);
+}
+
+sub _interpret_run_string {
+	my $thing= shift;
+	# If run is a string, and starts with SIG\w+, it's a killscript
+	if ($thing =~ /^SIG[A-Z0-9]+/) {
+		return { internal => [ 'killscript', split(/\s+/, $thing) ] };
+	}
+	# If run is a string, and starts with *WORD,
+	#  split on whitespace and make it an internal call
+	if ($thing =~ /^\*(\w+.*)/) {
+		return { internal => [ split(/\s+/, $1) ] };
+	}
+	# If run is a string, and contains shell characters,
+	#   convert it to call to $SHELL
+	if ($thing =~ /[^\w\s_=\/.:,-]/) {
+		return { exec => [ sub{$ENV{SHELL}}, sub{$ENV{SHELL}}, '-c', $thing ] };
+	}
+	# Else split on whitespace and make it the args to exec()
+	my @argv= split /\s+/, $thing;
+	@argv > 0 or die "Must have at least one argument\n";
+	return { exec => [ $argv[0], @argv ] };
 }
 
 1;
--- lib/App/Desd/Types.pm
+++ lib/App/Desd/Types.pm
@@ -25,9 +25,9 @@ declare 'MessageInstance', where { defined $_ and $_ =~ /^[0-9]+$/ };
 declare 'MessageField',    where { defined $_ and !($_ =~ /[\t\n]/) };
 declare 'MessageName',     where { defined $_ and $_ =~ /^[a-z0-9_]+$/ };
 declare 'CondVar',         where { defined $_ and blessed($_) and $_->can('recv') and $_->can('cb') };
-declare 'NonemptyArrayOfScalar', where {
+declare 'NonemptyArrayOfLazyScalar', where {
 	defined($_) and ref($_) eq 'ARRAY' and @$_ >= 1
-		and 0 == grep { ref $_ } @$_;
+		and 0 == grep { ref $_ && ref $_ ne 'CODE' } @$_;
 };
 
 1;
